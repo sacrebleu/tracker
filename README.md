98c50622f06a43edcdd30bf1ce0b2dd4d1204f7e
Jeremy Botha <mcfinnigan@gmail.com>
2022-01-28 14:26:39 +0000

    002 working 3s pause on motor step call; still need to consider how long it will take a motor to step but at 0.(3) Hz it shouldn't matter and can be tuned.

--- README.md
+++ README.md
@@ -7,10 +7,12 @@
 `tracker` is the command and control code that is intended to permit an Arduino Nano rev3 drive a stepper motor at a rate that will slew a telescope right ascension drive
 at the correct rate to track objects with reasonable accuracy for visual astronomy.
 
-The earth rotates at a rate of 1˚ every four minutes, which means that a telescope that has been correctly [Polar Aligned](https://skyandtelescope.org/astronomy-resources/accurate-polar-alignment/)
-is able to track celestial objects by rotating its Right Ascension axis opposite to the direction of Earth's rotation at the same angular rate.
+The Earth rotates at a rate of 1˚ every four minutes on its axis, which means that a telescope that has been correctly [Polar Aligned](https://skyandtelescope.org/astronomy-resources/accurate-polar-alignment/)
+is able to track celestial objects by rotating around its Right Ascension axis opposite to the direction of Earth's rotation at the same angular rate.
 
-1˚ every four minutes translates to a rotation rate of 0.00416(6) degrees per second on the RA axis, so some configuration of the program is required.
+1˚ every four minutes translates to a rotation rate of 0.00416(6) degrees per second on the RA axis.
+
+Given the variability of stepper motors and the inability to provide an exhaustive set, configuration values are used intead.
 
 #### Supported architectures
 
@@ -19,6 +21,8 @@ is able to track celestial objects by rotating its Right Ascension axis opposite
 #### Configuration
 
 `tracker` requires the following configuration:
+
+
 
 `degrees_per_step` - the number of degrees your chosen stepper motor will step per step
 
--- tracker.ino
+++ tracker.ino
@@ -1,8 +1,8 @@
 /* 
  *  reads input from an analog pin, and on v> v_crit it sets D13 to HIGH for 3 seconds.
  */
-// set debug to 0 to stop sysouts
-#define DEBUG 1
+// comment out this line to stop debugs
+#define DEBUG
 
 // state machine variables
 #define STATE_OFF 0
@@ -11,21 +11,27 @@
 #define STATE_FWD 1
 
 // LED output pins begin
-#define LED_REVERSE 6      // LED that lights when the drive is reversing
+#define LED_REVERSE 6  // LED that lights when the drive is reversing
 #define LED_TRACK 7    // LED that lights when the drive is tracking
 #define LED_FORWARD 8  // LED that lights when the drive is fast-forwarding
 // LED output pins end
+
+// button input pins
+#define BTN_REVERSE 2
+#define BTN_FORWARD 3
+#define BTN_TRACKING 9
+// button input pins end
+
+// MOTOR CONFIG
+#define EARTH_ROTATION 0.0041667 // approximation of 1/240
+#define DEGREES_PER_STEP 0.018 // motor step
+#define THRESHOLD 3000 // ms window length
 
 // motor output pins
 
 // motor output pins end
 
-// inputs
-#define BTN_REVERSE 2
-#define BTN_FORWARD 3
-#define BTN_TRACKING 9
-
-/* class model for an LED */
+/* class model for an LED - encodes the pin and provides utility methods to turn the LED on and off */
 class Led {
   private:
     byte pin;
@@ -56,14 +62,29 @@ class Led {
     }
 };
 
-/* model the system state in a single class */
+class AbstractMotor {
+    
+  public:
+    // construct a motor representation for `dps` degrees per step
+    AbstractMotor() {
+      
+    } 
+
+    virtual void drive_motor(int steps) { }
+};
+
+/* model the system state in a single class and provides utility methods for changing the state */
 class ControlSystem {
   private:
     Led reverse = Led(LED_REVERSE);
     Led forward = Led(LED_FORWARD);
     Led track   = Led(LED_TRACK);
+    AbstractMotor *motor;
 
     int state;
+    unsigned long start_time;
+    unsigned long end_time;
+    unsigned long window = 0;
     volatile bool interrupted = false;
     
   public:
@@ -73,6 +94,12 @@ class ControlSystem {
 
     void init() {
       set_off();
+      start_time = millis(); // read clock start time
+      end_time   = millis(); // read clock end time
+    }
+
+    void set_motor(AbstractMotor *motor) {
+      this->motor = motor; 
     }
 
     void set_off() {
@@ -107,7 +134,7 @@ class ControlSystem {
       return STATE_FWD == state;
     }
 
-    // this is not toggling correctly, needs more investigation into why.
+    // toggles between TRACKING and OFF
     void toggle_tracking(void) {
       if(digitalRead(BTN_TRACKING) == 0) {
         if(!this->is_interrupted()) {
@@ -127,23 +154,45 @@ class ControlSystem {
       }
     }    
 
+    // display the current state of the system
     void render() {
       reverse.set(is_reverse());
       forward.set(is_forward());
       track.set(is_tracking());
     }
 
+    // action tick
     void tick() {
+      start_time = end_time;
+      end_time   = millis();
+      window     += (end_time - start_time);
+      
       render();
+      step_motor();
       
       toggle_tracking();
-      
       clear_interrupt();
     }
 
+    void step_motor() {
+     if(window >= THRESHOLD) {
+        motor->drive_motor(steps());
+        window = 0;
+      }
+    }
+
+    int steps() {
+      return 1;
+    }
+
+    // output the state of the system if debugging is turned on
     void debug() {
-      Serial.print("Current ControlSystem state is: ");
-      Serial.println(state);
+      Serial.print("ø: ");
+      Serial.print(state);
+      Serial.print(" | dT : ");
+      Serial.print(end_time - start_time);
+      Serial.print(" | ∫ : ");
+      Serial.println(window);
     }
 
     bool is_interrupted() {
@@ -159,8 +208,59 @@ class ControlSystem {
     }
 };
 
+/*
+ * Encapsulate motor driver and timing information / calculations 
+ */
+class Motor : public AbstractMotor {
+  private:
+    ControlSystem *sys;
+  
+  public:
+    // construct a motor representation for `dps` degrees per step
+    Motor() { }
+
+    void set_controller(ControlSystem *controller) {
+      this->sys = controller; 
+    }
+
+    
+    /*
+     * Based on the gearing of the desired motor (1.8 deg / step) with a 1:51 planetary gearbox,
+     * and coupled to a 16t pinion driving a 45T gear on the RA pulley, the required frequency
+     * to drive the shaft at EARTH_ROTATION degrees / second will be ~ 1/3 Hz
+     * 
+     * This can be obtained by keeping a milisecond counter between start + end, and stepping the motor and resetting this counter when it exceeds 3000.
+     * For this to be reasonably accurate the main loop should execute approximately at 10Hz or better.  An error accumulator could be used to shorten the
+     * duration of subsequent steps perhaps.
+     * 
+     * Some sort of floating error accumulator that tries to tend back to zero might be appropriate.
+     *  
+     * Also needs to factor in tracking speed multipliers below 
+     *  
+     * Drives the primary telescope drive motor in accordance to the controller state machine where 1x 
+     * will counteract the rotation of the earth (approximately 1/4 degree a minute)
+     * 
+     * state    | drive speed
+     * -------------------
+     * OFF .    | 0
+     * FF .     | 5x 
+     * TRACKING | 1x
+     * REV .    | -1x
+     */
+    virtual void drive_motor(int steps) {
+        debug(steps);
+    }     
+
+    void debug(int steps) {
+      Serial.print("* Driving stepper ");
+      Serial.print(steps);
+      Serial.println(" steps.");
+    }
+};
+
 
 ControlSystem sys;
+Motor motor;
 
 // trigger on interrupt for reverse direction
 void set_rev(void) {
@@ -182,54 +282,6 @@ void set_ff(void) {
     sys.interrupt();
 }
 
-// outputs
-//int led_rev = 6;   // LED that lights when the drive is reversing
-//int led_trk = 7; // LED that lights when the drive is tracking
-//int led_fwd = 8; // LED that lights when the drive is fast-forwarding
-
-//int state = STATE_OFF;
-
-
-// investigate using some structs
-// single vars everywhere are messy
-
-/* 
- * short hand state setter 
- * int r - reverse led state
- * int t - track led state
- * int f - fast-forward led state
- */
-//void set_leds(int r, int t, int f) {
-//  digitalWrite(led_rev, r);
-//  digitalWrite(led_trk, t);
-//  digitalWrite(led_fwd, f);  
-//}
-
-// set the leds to indicate the current controller state
-//void show_state(int state) {
-//  sys.tick();
-// 
-//  interrupted=false;
-//}
-
-
-
-
-/*
- * Drives the primary telescope drive motor in accordance to the controller state machine where 1x 
- * will counteract the rotation of the earth (approximately 1/4 degree a minute)
- * 
- * state    | drive speed
- * -------------------
- * OFF .    | 0
- * FF .     | 5x 
- * TRACKING | 1x
- * REV .    | -1x
- */
-void drive_stepper(void) {
-  
-}
-
 void setup() {
   #ifdef DEBUG
   Serial.begin(9600);
@@ -240,6 +292,9 @@ void setup() {
 //  pinMode(led_fwd, OUTPUT);
 
   sys = ControlSystem();
+  motor = Motor();
+  sys.set_motor(&motor);
+  motor.set_controller(&sys);
   
 
   // set the three input pins
@@ -266,9 +321,9 @@ void loop(){
 
   // display button states
 //  show_state(state);
-  drive_stepper();
+//  drive_stepper(); // will pass control system here too
   // rotate the stepper motor in accordance with the system state
 
   // despam
-  delay(300);
+  delay(100);
 }
